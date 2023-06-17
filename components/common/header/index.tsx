80f3f28480de0bacebf5d8e352f2f724980486d3
Adil Peter <adilnesmith@ymail.com>
2023-06-17 11:52:13 +0500

    feat: header action button added

--- components/common/header/index.tsx
+++ components/common/header/index.tsx
@@ -2,11 +2,17 @@ import { FC } from 'react'
 import { HeaderProps } from 'lib/@types/common'
 import { PAGE_TITLE } from 'lib/constants/Header';
 import styles from './Header.module.scss'
+import Label from 'components/ui/label';
+import Button from 'components/ui/button';
 
 const Header: FC<HeaderProps> = ({ }) => {
     return (
         <header className={styles.header}>
             <h1 className={styles.header__title}>{PAGE_TITLE}</h1>
+            <div className={styles.header__actions}>
+                <Label type='header' test-dataid={"header-label"} children={"Rooms"} className={styles.header__actions__label} />
+                <Button buttonText={"Create New Room"} className={styles.header__actions__button}></Button>
+            </div>
         </header >
     )
 }
